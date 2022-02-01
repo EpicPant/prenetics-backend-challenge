85cd1c28f832046bfb75394677eb76acced67755
james_ycy <james.yip@prenetics.com>
2022-02-01 20:29:34 +0800

    seed data for tests

---
+++ src/seed/profile.seed.ts
@@ -0,0 +1,22 @@
+export const profileSeeds = [
+    {
+        name: 'Peter Chan',
+        organisation: null
+    },
+    {
+        name: 'Michael Caine',
+        organisation: null
+    },
+    {
+        name: 'Bruce Lee',
+        organisation: null
+    },
+    {
+        name: 'John Locke',
+        organisation: null
+    },
+    {
+        name: 'Andrea Lau',
+        organisation: null
+    }
+];
