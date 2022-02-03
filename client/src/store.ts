421a8565e1b9b238bd24f4856abde88ffcf1163f
james_ycy <james.yip@prenetics.com>
2022-02-03 18:11:24 +0800

    add pagination reducer to app store

--- client/src/store.ts
+++ client/src/store.ts
@@ -1,12 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { TypedUseSelectorHook, useSelector } from 'react-redux';
 import searchReducer from './feature/search';
+import paginationReducer from './feature/pagination';
 
 export const store = configureStore({
     reducer: {
-        search: searchReducer
+        search: searchReducer,
+        pagination: paginationReducer
     },
     
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export const useAppSelector:TypedUseSelectorHook<RootState> = useSelector;
