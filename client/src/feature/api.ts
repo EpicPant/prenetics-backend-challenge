14154873217d8c32c302b3fabe3b25bc9186bed4
james_ycy <james.yip@prenetics.com>
2022-02-07 13:35:18 +0800

    method to make request to api

---
+++ client/src/feature/api.ts
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import { RootState } from '../store';
+import { API_URL, ORGANISATION_ID } from '../resources/constants';
+import { ApiResponseType, ResultState } from '../resources/types';
+
+export const fetchResultsFormAPI = (state: RootState) => {
+    let uri = `${API_URL}/org/${ORGANISATION_ID}/sample?limit=${state.pagination.limit}&offset=${state.pagination.current_page - 1}&`;
+    uri = uri + new URLSearchParams({ ...state.search.query });
+    
+    return axios.get(uri)
+    .then((res): ResultState => {
+        console.log(res)
+        const { data, meta, included } = res.data as ApiResponseType;
+        console.log(data)
+        return {
+            status: state.result.status,
+            data: data.map((result, i) => {
+                return {
+                    patientName: included[i].attributes.name,
+                    sampleId: result.attributes.sampleId,
+                    result: result.attributes.result || '',
+                    resultType: result.attributes.resultType,
+                    resultTime: new Date(result.attributes.resultTime).toLocaleString(),
+                    activationTime: new Date(result.attributes.activateTime).toLocaleString()
+                }
+            }),
+            meta: {
+                total: meta.total
+            }
+        }
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+};
